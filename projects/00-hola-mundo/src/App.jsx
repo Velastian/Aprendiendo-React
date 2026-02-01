42dbea3df3ba3b031f30c9c45fa570669dfce953
Velastian <cristian13vp@hotmail.com>
2026-02-01 17:57:49 -0600

    Add: primera clase
    En esta primera clase se creo un componente reutilizable de una tarjeta
    de x.com

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Heraldo',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hernandez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App() {
+  return (
+    <section className='App'>
+      {
+        users.map(user => {
+          const { userName, name, isFollowing } = user
+          return (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}>
+              {name}
+            </TwitterFollowCard>
+          )
+        })
+      }
+    </section >
+  )
+}
+
