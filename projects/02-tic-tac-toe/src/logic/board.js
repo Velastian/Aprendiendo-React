79d1d8eca74144cb06f87c07f710606a61b71b23
Velastian <cristian13vp@hotmail.com>
2026-02-02 01:13:46 -0600

    Feat: Extract to components Tic Tac Toe

---
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,25 @@
+import { WINNER_COMBOS } from "../constants.js"
+
+export const checkWinnerFrom = (boardToCheck) => {
+  // Revisamos todas las combinaciones ganadoras
+  // para ver si x u o gano
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  // Si no hay ganador
+  return null
+}
+
+export const checkEndGame = (newBoard) => {
+  // revisamos si hay un empate
+  // si hay mas espacios vacios
+  // en el tablero
+  return newBoard.every((square) => square != null)
+}
